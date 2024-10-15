b14094d0968184adcaf6bf6f2042326c11eae660
velonisava25 <avelonis5940@stu.neisd.net>
2024-10-15 11:07:38 -0500

    This is my first commit!

---
+++ ExamplePython.py
@@ -0,0 +1,10 @@
+
+a = "String 1"
+b = "String 2"
+print("a =" + a + " and  b=" + b)
+
+x = "String 3"
+y = "String 4"
+print(f"x = {x} and y = {y}")
+
+print("c = {c} and d = {d}".format(c=1, d=2))
